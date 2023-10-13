2ac4f37373129efa80c20843dde0ff6898832938
Luca Conesa <conesa.luca1997@gmail.com>
2023-10-13 10:04:39 -0300

    agregue funciones y elimine dom,deje 4 eventos

--- main.js
+++ main.js
@@ -1,142 +1,157 @@
-let attempts = 10; // Intentos iniciales
-let score = 0; // Puntuación del jugador
-const guessHistory = []; // Historial de adivinanzas
-let timer; // Temporizador
-let timeLeft = 10; // Tiempo restante en segundos
-let successfulAttempts = 0; // Intentos exitosos
-let difficulty = "medio"; // Nivel de dificultad (por defecto: medio)
+let intentos = 10; // Intentos iniciales
+let puntaje = 0; // Puntuación del jugador
+const historialSuposiciones = []; // Historial de suposiciones
+let intentosExitosos = 0; // Suposiciones correctas
+let dificultad = "medio"; // Nivel de dificultad (por defecto: medio)
+let tiempoRestante = 10; // Tiempo restante en segundos
+let temporizador; // Temporizador
 
-function generateRandomNumber(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function setMessage(message) {
-    document.getElementById("message").textContent = message;
-}
-
-function showImage(isCorrect) {
-    const correctImage = document.getElementById("correct-image");
-    const wrongImage = document.getElementById("wrong-image");
-    correctImage.style.display = isCorrect ? "block" : "none";
-    wrongImage.style.display = isCorrect ? "none" : "block";
-}
-
-function updateGuessHistory(guess, isCorrect) {
-    guessHistory.push({ guess, isCorrect });
-    renderGuessHistory();
-}
-
-function renderGuessHistory() {
-    const guessHistoryElement = document.getElementById("guessHistory");
-    guessHistoryElement.innerHTML = `<strong>Historial de Adivinanzas:</strong><br>`;
-    guessHistory.forEach((item, index) => {
-        guessHistoryElement.innerHTML += `Intento ${index + 1}: ${item.guess} (${item.isCorrect ? "Correcto" : "Incorrecto"})<br>`;
+function mostrarMensajeSweetAlert(mensaje) {
+    Swal.fire({
+        title: 'Mensaje',
+        text: mensaje,
+        icon: 'info',
+        confirmButtonText: 'OK'
     });
 }
 
-function updateSuccessfulAttempts() {
-    document.getElementById("successfulAttempts").textContent = `Intentos Exitosos: ${successfulAttempts}`;
+// Ejemplo de uso
+mostrarMensajeSweetAlert("¡Hola!soy la computadora y te reto a que adivines el numero en el que estoy pensando,si lo adivinas 3 veces te doy un certificado que diga que me ganaste pero lo dudo muajajaja.                            APRETA OK Y LUEGO ELEGIR NUMERO PARA COMENZAR");
+
+function generarNumeroAleatorio(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function startTimer() {
-    clearInterval(timer);
-    timer = setInterval(() => {
-        if (timeLeft > 0) {
-            timeLeft--;
-            document.getElementById("timeLeft").textContent = `Tiempo restante: ${timeLeft}`;
+function mostrarMensaje(mensaje) {
+    document.getElementById("message").textContent = mensaje;
+}
+
+function mostrarImagen(esCorrecta) {
+    const imagenCorrecta = document.getElementById("correct-image");
+    const imagenIncorrecta = document.getElementById("wrong-image");
+    imagenCorrecta.style.display = esCorrecta ? "block" : "none";
+    imagenIncorrecta.style.display = esCorrecta ? "none" : "block";
+}
+
+function actualizarHistorialSuposiciones(suposicion, esCorrecta) {
+    historialSuposiciones.push({ suposicion, esCorrecta });
+    renderizarHistorialSuposiciones();
+}
+
+function renderizarHistorialSuposiciones() {
+    const historialSuposicionesElemento = document.getElementById("guessHistory");
+    historialSuposicionesElemento.innerHTML = `<strong>Historial de Suposiciones:</strong><br>`;
+    historialSuposiciones.forEach((item, index) => {
+        historialSuposicionesElemento.innerHTML += `Suposición ${index + 1}: ${item.suposicion} (${item.esCorrecta ? "Correcta" : "Incorrecta"})<br>`;
+    });
+}
+
+function actualizarIntentosExitosos() {
+    document.getElementById("successfulAttempts").textContent = `Suposiciones Correctas: ${intentosExitosos}`;
+}
+
+function iniciarTemporizador() {
+    clearInterval(temporizador);
+    temporizador = setInterval(() => {
+        if (tiempoRestante > 0) {
+            tiempoRestante--;
+            document.getElementById("timeLeft").textContent = `Tiempo Restante: ${tiempoRestante}`;
         } else {
-            attempts--;
-            document.getElementById("attempts").textContent = `Intentos restantes: ${attempts}`;
-            timeLeft = 10;
-            document.getElementById("timeLeft").textContent = `Tiempo restante: ${timeLeft}`;
-            const computerGuess = generateRandomNumber(1, 15);
-            setMessage("Se agotó el tiempo. ¡Se redujo un intento!");
-            updateGuessHistory(-1, false);
-            showImage(false);
-            document.getElementById("result").textContent = `La computadora eligió: ${computerGuess}`;
-            if (attempts === 0) {
-                endGame();
+            intentos--;
+            document.getElementById("attempts").textContent = `Intentos Restantes: ${intentos}`;
+            tiempoRestante = 10;
+            document.getElementById("timeLeft").textContent = `Tiempo Restante: ${tiempoRestante}`;
+            const suposicionComputadora = generarNumeroAleatorio(1, 15);
+            mostrarMensaje("Se ha agotado el tiempo. ¡Se ha reducido un intento!");
+            actualizarHistorialSuposiciones(-1, false);
+            mostrarImagen(false);
+            document.getElementById("result").textContent = `La computadora eligió: ${suposicionComputadora}`;
+            if (intentos === 0) {
+                finalizarJuego();
             }
         }
     }, 1000);
 }
 
-function checkGuess() {
-    const guess = parseInt(prompt("Ingresa tu suposición (un número entre 1 y 15):"));
-    const resultElement = document.getElementById("result");
+function verificarSuposicion() {
+    const suposicionTexto = prompt("Ingresa tu suposición (un número entre 1 y 15):");
+    const suposicion = parseInt(suposicionTexto);
+    const resultadoElemento = document.getElementById("result");
 
-    if (isNaN(guess) || guess < 1 || guess > 15) {
-        setMessage("Ingresa un número válido entre 1 y 15.");
+    if (isNaN(suposicion) || suposicion < 1 || suposicion > 15) {
+        mostrarMensaje("Ingresa un número válido entre 1 y 15.");
     } else {
-        clearInterval(timer);
-        timeLeft = 10;
-        document.getElementById("timeLeft").textContent = `Tiempo restante: ${timeLeft}`;
+        clearInterval(temporizador);
+        tiempoRestante = 10;
+        document.getElementById("timeLeft").textContent = `Tiempo Restante: ${tiempoRestante}`;
 
-        const computerGuess = generateRandomNumber(1, 15);
+        const suposicionComputadora = generarNumeroAleatorio(1, 15);
 
-        if (guess === computerGuess) {
-            score++;
-            setMessage("¡Felicidades! Has adivinado el número.");
-            showImage(true);
-            successfulAttempts++;
-        } else if (guess < computerGuess) {
-            setMessage("El número es mayor. Intenta de nuevo.");
-            showImage(false);
+        if (suposicion === suposicionComputadora) {
+            puntaje++;
+            mostrarMensaje("¡Felicidades! Has adivinado el número.");
+            mostrarImagen(true);
+            intentosExitosos++;
+        } else if (suposicion < suposicionComputadora) {
+            mostrarMensaje("El número es mayor. Inténtalo de nuevo.");
+            mostrarImagen(false);
         } else {
-            setMessage("El número es menor. Intenta de nuevo.");
-            showImage(false);
+            mostrarMensaje("El número es menor. Inténtalo de nuevo.");
+            mostrarImagen(false);
         }
 
-        resultElement.textContent = `La computadora eligió: ${computerGuess}`;
+        resultadoElemento.textContent = `La computadora eligió: ${suposicionComputadora}`;
 
-        updateGuessHistory(guess, guess === computerGuess);
-        updateSuccessfulAttempts();
+        actualizarHistorialSuposiciones(suposicion, suposicion === suposicionComputadora);
+        actualizarIntentosExitosos();
 
-        if (attempts === 0) {
-            endGame();
+        intentos--;
+        document.getElementById("score").textContent = `Puntuación: ${puntaje}`;
+        document.getElementById("attempts").textContent = `Intentos Restantes: ${intentos}`;
+
+        if (intentos === 0) {
+            finalizarJuego();
         } else {
-            startTimer();
+            iniciarTemporizador();
         }
     }
-
-    document.getElementById("score").textContent = `Puntuación: ${score}`;
-    document.getElementById("attempts").textContent = `Intentos restantes: ${attempts}`;
 }
 
-function endGame() {
-    setMessage(`¡Agotaste tus ${score} intentos! El número de la computadora era ${generateRandomNumber(1, 15)}.`);
+function finalizarJuego() {
+    mostrarMensaje(`¡Has agotado tus ${puntaje} intentos! El número de la computadora era ${generarNumeroAleatorio(1, 15)}.`);
     document.getElementById("checkButton").disabled = true;
-    clearInterval(timer);
+    clearInterval(temporizador);
 }
 
-function resetGame() {
-    attempts = 10;
-    score = 0;
-    guessHistory.length = 0;
-    successfulAttempts = 0;
-    document.getElementById("guess").value = "";
-    setMessage("");
+function reiniciarJuego() {
+    intentos = 10;
+    puntaje = 0;
+    historialSuposiciones.length = 0;
+    intentosExitosos = 0;
+    document.getElementById("guessInput").value = "";
+    mostrarMensaje("");
     document.getElementById("result").textContent = "";
     document.getElementById("score").textContent = "Puntuación: 0";
-    document.getElementById("attempts").textContent = `Intentos restantes: ${attempts}`;
+    document.getElementById("attempts").textContent = `Intentos Restantes: ${intentos}`;
     document.getElementById("checkButton").disabled = false;
-    document.getElementById("correct-image").style.display = "none";
-    document.getElementById("wrong-image").style.display = "none";
-    renderGuessHistory();
-    timeLeft = 10;
-    document.getElementById("timeLeft").textContent = `Tiempo restante: ${timeLeft}`;
+    renderizarHistorialSuposiciones();
+    tiempoRestante = 10;
+    document.getElementById("timeLeft").textContent = `Tiempo Restante: ${tiempoRestante}`;
 }
 
-function getHint() {
-    const computerGuess = generateRandomNumber(1, 15);
-    const isEven = computerGuess % 2 === 0;
-    const hintMessage = isEven ? "El número es par." : "El número es impar.";
-    setMessage(hintMessage);
+function obtenerPista() {
+    const suposicionComputadora = generarNumeroAleatorio(1, 15);
+    const esPar = suposicionComputadora % 2 === 0;
+    const mensajePista = esPar ? "El número es par." : "El número es impar.";
+    mostrarMensaje(mensajePista);
 }
 
-document.getElementById("checkButton").addEventListener("click", checkGuess);
-document.getElementById("resetButton").addEventListener("click", resetGame);
-document.getElementById("hintButton").addEventListener("click", getHint);
+document.getElementById("checkButton").addEventListener("click", verificarSuposicion);
+document.getElementById("resetButton").addEventListener("click", reiniciarJuego);
+document.getElementById("hintButton").addEventListener("click", obtenerPista);
+document.getElementById("difficultySelect").addEventListener("change", function() {
+    dificultad = document.getElementById("difficultySelect").value;
+    reiniciarJuego();
+});
 
-// Agregar evento para reiniciar el juego al cargar la página
-window.addEventListener("load", resetGame);
+reiniciarJuego();
